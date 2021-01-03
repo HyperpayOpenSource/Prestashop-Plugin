026caed107f191a2fa9dfe7c4e727c6602023110
Ibraheem Jarrar <ibraheem.jarrar@hyperpay.com>
2021-01-03 16:28:42 +0200

    Add mada blackBins

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -49,7 +49,7 @@ class HyperpayValidationModuleFrontController extends ModuleFrontController
         $total = (float) $cart->getOrderTotal(true, Cart::BOTH);
 
         // after all checks are successfull we send the get payment status
-        // also all these checks need to be done before  creating the iframe 
+        // also all these checks need to be done before  creating the iframe
         $paymentStatus =  json_decode(Request::getPaymentStatus($settingsKey, Tools::getValue('id')), true);
 
         $status = "";
@@ -65,6 +65,20 @@ class HyperpayValidationModuleFrontController extends ModuleFrontController
             $errors['code'] = $paymentStatus['result']['code'];
             $errors['description'] = isset($paymentStatus['result']) && isset($paymentStatus['result']['description']) ? $paymentStatus['result']['description'] : "missing description";
             $status = 'fail';
+
+            if (isset($paymentStatus['card']['bin'])) {
+              $blackBins = $this->getMadaBlackBins();
+              $searchBin = $paymentStatus['card']['bin'];
+              if (in_array($searchBin,$blackBins)) {
+                if ($this->context->language->iso_code == 'ar') {
+                  $errors['description'] = 'عذرا! يرجى اختيار خيار الدفع "مدى" لإتمام عملية الشراء بنجاح.';
+                }else{
+                  $errors['description'] = 'Sorry! Please select "mada" payment option in order to be able to complete your purchase successfully.';
+                }
+
+            }
+          }
+          
         }
 
         if ($status == 'success') {
@@ -120,4 +134,106 @@ class HyperpayValidationModuleFrontController extends ModuleFrontController
             }
         }
     }
+
+    private function getMadaBlackBins()
+{
+  return array(
+    "588845",
+    "440647",
+    "440795",
+    "446404",
+    "457865",
+    "968208",
+    "588846",
+    "493428",
+    "539931",
+    "558848",
+    "557606",
+    "968210",
+    "636120",
+    "417633",
+    "468540",
+    "468541",
+    "468542",
+    "468543",
+    "968201",
+    "446393",
+    "409201",
+    "458456",
+    "484783",
+    "968205",
+    "462220",
+    "455708",
+    "588848",
+    "455036",
+    "968203",
+    "486094",
+    "486095",
+    "486096",
+    "504300",
+    "440533",
+    "489318",
+    "489319",
+    "445564",
+    "968211",
+    "401757",
+    "410685",
+    "406996",
+    "432328",
+    "428671",
+    "428672",
+    "428673",
+    "968206",
+    "446672",
+    "543357",
+    "434107",
+    "407197",
+    "407395",
+    "412565",
+    "431361",
+    "604906",
+    "521076",
+    "588850",
+    "968202",
+    "529415",
+    "535825",
+    "543085",
+    "524130",
+    "554180",
+    "549760",
+    "588849",
+    "968209",
+    "524514",
+    "529741",
+    "537767",
+    "535989",
+    "536023",
+    "513213",
+    "520058",
+    "585265",
+    "588983",
+    "588982",
+    "589005",
+    "508160",
+    "531095",
+    "530906",
+    "532013",
+    "605141",
+    "968204",
+    "422817",
+    "422818",
+    "422819",
+    "428331",
+    "483010",
+    "483011",
+    "483012",
+    "589206",
+    "968207",
+    "419593",
+    "439954",
+    "530060",
+    "531196"
+  );
+}
+
 }
